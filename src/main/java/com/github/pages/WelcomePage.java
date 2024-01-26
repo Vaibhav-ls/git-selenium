6d0ea6fc4dc5d91f90aae7e0ef29ccf3c93844a9
Vaibhav-ls <vaibhav.mhetre1010@gmail.com>
2024-01-26 11:08:34 +0530

    Project structure has been created

---
+++ src/main/java/com/github/pages/WelcomePage.java
@@ -0,0 +1,12 @@
+package com.github.pages;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.github.base.SeleniumBase;
+
+public class WelcomePage extends SeleniumBase{
+
+	public WelcomePage(RemoteWebDriver driver) {
+		this.driver = driver;
+	}
+}
